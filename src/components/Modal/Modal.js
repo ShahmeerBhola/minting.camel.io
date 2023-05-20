73b31a684c9e106f4d963024b76603d23a19e813
ShahmeerBhola <shahmir.shamim1@gmail.com>
2023-05-21 01:25:42 +0500

    generated link

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAccount } from "wagmi";
 import { Cancel, Coin } from "../../assets/images";
 import "./Modal.css";
 
 export const Modal = ({ onClose, show }) => {
+  const { address } = useAccount();
+  // const baseUrl = "http://localhost:3000/contract";
+  const baseUrl = "https://referral-front-end.vercel.app/contract";
+  const [walletAddress, setWalletAddress] = useState("");
+  const linkHandler = () => {
+    setWalletAddress(address);
+  };
+  const copyHandler = () => {
+    const copylink = `${baseUrl}?wallet=${walletAddress}`;
+    navigator.clipboard.writeText(copylink);
+    onClose?.();
+  };
   if (!show) {
     return null;
   }
@@ -24,16 +37,31 @@ export const Modal = ({ onClose, show }) => {
             <li>2. Get your unique Refer & Earn link.</li>
             <li>3. Share the link with your audience.</li>
             <li>
-              4. Earn a 5% commission on each NFT sale made through your link.
+              4. Earn a 10% commission on each NFT sale made through your link.
             </li>
             <li>5. Commissions are automatically credited to your wallet.</li>
             <li>6. Unlimited earning potential!</li>
           </ol>
           <div className="modal-btn">
-            <button className="modal-generate">
-              <span>Generate Link</span>
-            </button>
-            <div className="generated-link"></div>
+            {walletAddress == "" ? (
+              <button className="modal-generate" onClick={linkHandler}>
+                <span>Generate Link</span>
+              </button>
+            ) : (
+              <button
+                className="modal-generate"
+                onClick={copyHandler}
+                style={{
+                  background:
+                    "radial-gradient(50% 50% at 50% 50%, #5095FD 0%, #005B9C 100%)",
+                }}
+              >
+                <span style={{ color: "#F4F5F7" }}>COPY LINK</span>
+              </button>
+            )}
+            <div className="generated-link">
+              <span>{walletAddress && walletAddress}</span>
+            </div>
           </div>
         </div>
       </div>
